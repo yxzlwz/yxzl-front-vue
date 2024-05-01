36ca2300ae3a8c925e03407902b3148776f58d6c
yxzlwz <danny070601@aliyun.com>
2024-05-01 18:41:02 +0800

    [fix]

--- test.js
+++
@@ -1,6 +0,0 @@
-import { TOTP } from "totp-generator"
-
-// Keys provided must be base32 strings, ie. only containing characters matching (A-Z, 2-7, =).
-const { otp, expires } = TOTP.generate("DNEMGCWDHFARJDRAT4PCMXJ6KZ7HPS5A")
-
-console.log(otp) 
